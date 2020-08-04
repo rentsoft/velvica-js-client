6994941299b2c1b8470dfdc587bfbc8d03b77c99
igor-vodka <uhehesh@gmail.com>
2020-08-04 17:31:03 +0300

    Add fetcher to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import {ControlPanelApi, VPSLogFilter, VPSStateChange} from "./ControlPanelApi";
 import {PartnerApi} from "./PartnerApi";
+import Fetcher from "./util/Fetcher";
 
 export {
   ControlPanelApi,
   VPSLogFilter,
   VPSStateChange,
-  PartnerApi
+  PartnerApi,
+  Fetcher
 };
