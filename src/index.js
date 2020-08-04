a2257a4cb472be625384b77248a5602b62d215cf
igor-vodka <uhehesh@gmail.com>
2020-08-04 17:26:08 +0300

    Create index.js with indexed exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {ControlPanelApi, VPSLogFilter, VPSStateChange} from "./ControlPanelApi";
+import {PartnerApi} from "./PartnerApi";
+
+export {
+  ControlPanelApi,
+  VPSLogFilter,
+  VPSStateChange,
+  PartnerApi
+};
