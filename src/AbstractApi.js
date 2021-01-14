fbe62ef8c587ba2f14a753c2c68c3d969b9a2af1
Igor Vodka <uhehesh@gmail.com>
2021-01-14 16:10:32 +0300

    Update src/AbstractApi.js
    Co-authored-by: Alexey Zakharov <24409809+koshker@users.noreply.github.com>

--- src/AbstractApi.js
+++ src/AbstractApi.js
@@ -75,8 +75,7 @@ export default class AbstractApi {
       return path;
     }
 
-    const requestPathContainsGetParams = path.includes('?');
-    const glueSign = requestPathContainsGetParams ? '&' : '?';
+    const glueSign = path.includes('?') ? '&' : '?';
 
     return `${path}${glueSign}${params}`;
   }
