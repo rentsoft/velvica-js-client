9db2f7d83a9f63144704d6c473190df1a38ff1fe
igor-vodka <uhehesh@gmail.com>
2021-05-13 11:49:27 +0300

    Move BoApi away

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {ControlPanelApi, VPSLogFilter, VPSStateChange} from "./ControlPanelApi";
 import {PartnerApi} from "./PartnerApi";
-import {BoApi} from "./BoApi";
 import {AgentTypes, DiscountStatuses, DiscountStatusesForUser, PersonalCodeStatuses, ServerStatuses} from "./enum";
 import Fetcher from "./util/Fetcher";
 import Options from "./util/Options";
@@ -10,7 +9,6 @@ export {
   VPSLogFilter,
   VPSStateChange,
   PartnerApi,
-  BoApi,
   AgentTypes,
   ServerStatuses,
   DiscountStatuses,
--- tests/util.js
+++ tests/util.js
@@ -1,4 +1,7 @@
 import FormData from 'formdata-node';
+import {before, it} from "mocha";
+import {BoApi} from "../src";
+import {expect} from "chai";
 global.FormData = FormData;
 
 export const createFetcherStub = () => {
@@ -18,4 +21,35 @@ export const createFetcherStub = () => {
       };
     }
   };
+};
+
+export const launchTests = (apiMaker, replacements, tests) => {
+  let api = null;
+
+  before(() => {
+    process.env.NODE_ENV = 'production';
+  });
+
+  Object.entries(tests).forEach(([testName, {action, expected, error}]) => {
+    it(testName, async () => {
+      const fetcher = createFetcherStub();
+      api = new apiMaker(fetcher);
+
+      if (error) {
+        action(api).catch(err => expect(err.message).to.equal(error));
+      } else {
+        await action(api);
+
+        const replacedUrl = Object.entries(replacements).reduce(
+          (url, [search, replacement]) => url.replace(search, replacement),
+          expected.url
+        );
+
+        expect(fetcher.result).deep.equal({
+          url: replacedUrl,
+          params: expected.params
+        });
+      }
+    });
+  });
 };
